60b1d47ad5d3bca842ad8860b951fb1c940e4e13
Damian Gryski <damian@gryski.com>
2013-06-03 21:27:38 +0200

    improve tests

--- cobs_test.go
+++ cobs_test.go
@@ -3,6 +3,7 @@ package cobs
 import (
 	"bytes"
 	"testing"
+	"testing/quick"
 )
 
 var encodeTests = []struct {
@@ -25,15 +26,70 @@ var encodeTests = []struct {
 
 func TestEncode(t *testing.T) {
 
-    for _, tst := range encodeTests {
-        o := Encode(tst.in)
-        if !bytes.Equal(o, tst.out) {
-            t.Errorf("encode failed: got % 02x wanted % 02x\n", o, tst.out)
-        }
+	for _, tst := range encodeTests {
+		o := Encode(tst.in)
+		if !bytes.Equal(o, tst.out) {
+			t.Errorf("encode failed: got % 02x wanted % 02x\n", o, tst.out)
+		}
 
-        o = Decode(o)
-        if !bytes.Equal(o, tst.in) {
-            t.Errorf("decode failed: got % 02x wanted % 02x\n", o, tst.in)
-        }
-    }
+		o = Decode(o)
+		if !bytes.Equal(o, tst.in) {
+			t.Errorf("decode failed: got % 02x wanted % 02x\n", o, tst.in)
+		}
+	}
+}
+
+var encodeZPETests = []struct {
+	in  []byte
+	out []byte
+}{
+	{
+		[]byte{0x45, 0x00, 0x00, 0x2C, 0x4C, 0x79, 0x00, 0x00, 0x40, 0x06, 0x4F, 0x37},
+		[]byte{0xE2, 0x45, 0xE4, 0x2C, 0x4C, 0x79, 0x05, 0x40, 0x06, 0x4F, 0x37},
+	},
+	{
+		[]byte{0x11, 0x00, 0x00, 0x00},
+		[]byte{0xE2, 0x11, 0xE1},
+	},
+	{
+		[]byte{0x11, 0x22, 0x00, 0x33},
+		[]byte{0x03, 0x11, 0x22, 0x02, 0x33},
+	},
+}
+
+func TestEncodeZPE(t *testing.T) {
+
+	for _, tst := range encodeZPETests {
+		o := EncodeZPE(tst.in)
+		if !bytes.Equal(o, tst.out) {
+			t.Errorf("encode zpe failed: got % 02x wanted % 02x\n", o, tst.out)
+		}
+
+		o = DecodeZPE(o)
+		if !bytes.Equal(o, tst.in) {
+			t.Errorf("decode zpe failed: got % 02x wanted % 02x\n", o, tst.in)
+		}
+	}
+}
+
+func TestCobsQuick(t *testing.T) {
+
+	f := func(s []byte) bool {
+		e := Encode(s)
+		o := Decode(e)
+		return bytes.Equal(s, o)
+	}
+
+	quick.Check(f, nil)
+}
+
+func TestCobsZPEQuick(t *testing.T) {
+
+	f := func(s []byte) bool {
+		e := EncodeZPE(s)
+		o := DecodeZPE(e)
+		return bytes.Equal(s, o)
+	}
+
+	quick.Check(f, nil)
 }
