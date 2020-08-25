782832568c21bac7bae9b7d4cf4a1d601416ec7c
Damian Gryski <damian@gryski.com>
2020-08-25 12:02:43 -0700

    update fuzz function

--- fuzz.go
+++ fuzz.go
@@ -2,11 +2,43 @@
 
 package cobs
 
+import (
+	"bytes"
+	"fmt"
+)
+
 func Fuzz(data []byte) int {
-	// _, err := Decode(data)
-	_, err := DecodeZPE(data)
-	if err != nil {
-		return 0
+
+	enc := Encode(data)
+
+	if bytes.IndexByte(enc, 0) != -1 {
+		panic("encoded has a 0")
 	}
-	return 1
+
+	orig, err := Decode(enc)
+	if err != nil {
+		panic("failed decode encoded data")
+	}
+
+	if !bytes.Equal(data, orig) {
+		panic("faied to roundtrip" + fmt.Sprintf("%q != %q", data, orig))
+	}
+
+	zenc := EncodeZPE(data)
+	if bytes.IndexByte(zenc, 0) != -1 {
+		panic("zpe encoded has a 0")
+	}
+	zorig, err := DecodeZPE(zenc)
+	if err != nil {
+		panic("faied to zpe decode")
+	}
+
+	if !bytes.Equal(data, zorig) {
+		panic("faied to zpe roundtrip" + fmt.Sprintf("%q != %q", data, zorig))
+	}
+
+	Decode(data)
+	DecodeZPE(data)
+
+	return 0
 }
